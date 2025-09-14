ad655ca919e53854af25be24161fc84e60e55ed8
Erica Hoerschgen <changeofheirt@gmail.com>
2025-09-14 16:45:58 -0500

    final push

--- functions.php
+++ functions.php
@@ -1,13 +1,15 @@
 <?php
     function checkIfOverdue($dueDate, $returnDate){
-        $dueDate = strtotime($dueDate);
-        $returnDate = strtotime($returnDate);
+        $dueDate = date_create($dueDate);
+        $returnDate = date_create($returnDate);
         if($dueDate > $returnDate){
-            $untilDue = $dueDate - $returnDate;
-            return "Returned on time. Time until due: " . date("j", $untilDue)  . " day(s).";
+            $untilDue = date_diff($dueDate, $returnDate);
+            // return "Returned on time. Time until due: " . date("j", $untilDue)  . " day(s).";
+            return $untilDue->format("Returned on time. Time until due: %a day(s).");
         } if ($dueDate < $returnDate) {
-            $overdue = $returnDate - $dueDate;
-            return "Returned late. Time overdue: " . date("j", $overdue) . " day(s).";
+            $overdue = date_diff($returnDate, $dueDate);
+            // return "Returned late. Time overdue: " . date("j", $overdue) . " day(s).";
+            return $overdue->format("Returned late. Time overdue: %a day(s).");
         } else {
             return "The due date is today.";
         }
--- index.php
+++ index.php
@@ -9,28 +9,24 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Library Book Return</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
-    <style>
-        body{
-            width: 80%;
-            margin: auto;
-            margin-top: 25px;
-        }
-    </style>
+    <link href="styles.css", rel="stylesheet">
 </head>
 <body>
-    <?php include "form.php" ?>
+    <div>
+        <?php include "form.php" ?>
 
-    <?php
-        if($_GET){
-            $dueDate = $_GET["dueDate"];
-            $returnDate = $_GET["returnDate"];
-            $isOverdue = checkIfOverdue($dueDate, $returnDate);
-            echo("<br>");
-            echo("<h1>$isOverdue</h1>");
-            echo("<p>Due date: $dueDate</p>");
-            echo("<p>Return date: $returnDate</p>");
-        }
-    ?>
+        <?php
+            if($_GET){
+                $dueDate = $_GET["dueDate"];
+                $returnDate = $_GET["returnDate"];
+                $isOverdue = checkIfOverdue($dueDate, $returnDate);
+                echo("<br>");
+                echo("<h1>$isOverdue</h1>");
+                echo("<p>Due date: $dueDate</p>");
+                echo("<p>Return date: $returnDate</p>");
+            }
+        ?>
+    </div>
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js" integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI" crossorigin="anonymous"></script>
 </body>
